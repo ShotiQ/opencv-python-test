23483a2e8ca637feae1ea886ce0d9399dc38131e
Quintos, Carlos M. S <carlos.m.s.quintos@accenture.com>
2018-06-14 10:00:14 +0800

    chore: add more image_filters

--- notebooks/3-image-filter.py
+++ notebooks/3-image-filter.py
@@ -13,7 +13,8 @@ def apply_invert(frame):
 
 def apply_sepia(frame, intensity = 0.5):
     blue, green, red = 20, 66, 112
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_convert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     sepia_bgra = (blue, green, red, 1)
     
@@ -35,6 +36,47 @@ def apply_burgundy(frame, intensity = 0.5):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
     return frame
 
+def apply_alpha_convert(frame):
+    try:
+        frame.shape[3]
+    except IndexError:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        return frame
+
+def apply_portrait_mode_mask(frame):
+    frame = apply_alpha_convert(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+    
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    
+    return mask
+    
+def apply_portrait_mode_blurred(frame):
+    frame = apply_alpha_convert(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    blurred = cv2.GaussianBlur(frame, (21,21), 0)
+    
+    return blurred
+
+def apply_portrait_mode_blend(frame):
+    frame = apply_alpha_convert(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    blurred = cv2.GaussianBlur(frame, (21,21), 0)
+    
+    blended = apply_blend(frame, blurred, mask)
+    frame = cv2.cvtColor(blended, cv2.COLOR_BGRA2BGR)
+    return frame
+
+def apply_blend(frame_1, frame_2, mask):
+    alpha = mask / 255.0
+    blended = cv2.convertScaleAbs(frame_1 * (1 - alpha) + (frame_2 * alpha))
+    return blended
+
 def show_webcam():
     cap = cv2.VideoCapture(0)
     while True:
@@ -43,13 +85,17 @@ def show_webcam():
         invert = apply_invert(frame)
         sepia = apply_sepia(frame)        
         bur = apply_burgundy(frame)
-
+        blur = apply_portrait_mode_blurred(frame)
+        gray = apply_portrait_mode_mask(frame)
+        blend = apply_portrait_mode_blend(frame)
         
         cv2.imshow('true', frame)
         cv2.imshow('invert', invert)
         cv2.imshow('sep', sepia)
         cv2.imshow('bur', bur)
-
+        cv2.imshow('blur', blur)
+        cv2.imshow('gray', gray)
+        cv2.imshow('blend', blend)
 
         if cv2.waitKey(1) == 27:        
             cap.release()
